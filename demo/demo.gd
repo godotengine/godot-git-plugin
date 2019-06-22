6c2cd289bf9411aaa5aad79a1a02c6183fe72a7b
Twarit <wtwarit@gmail.com>
2019-06-22 13:45:46 +0530

    Get simple gdnative library to be used properly

--- demo/demo.gd
+++ demo/demo.gd
@@ -1,13 +1,9 @@
-extends Node
+extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# load the SIMPLE library
+const SIMPLE = preload("res://bin/test.gdns")
+onready var data = SIMPLE.new()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Button_pressed():
+	$Label.text = "Data = " + data.get_data()
+	
