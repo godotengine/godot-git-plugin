08a74bfb61995216294d83fce37c5f3694f8b0b6
Twarit <wtwarit@gmail.com>
2019-06-25 22:56:22 +0530

    Clean up C code

--- demo/demo.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-# load the SIMPLE library
-const SIMPLE = preload("res://bin/test.gdns")
-onready var data = SIMPLE.new()
-
-func _on_Button_pressed():
-	$Label.text = "Data = " + data.get_data()
-	
