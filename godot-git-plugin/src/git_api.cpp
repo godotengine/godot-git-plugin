f6dc2a614f70740abb77f9d57566329d34339145
Twarit <wtwarit@gmail.com>
2019-07-30 23:55:05 +0530

    Add support for proxy end-point functions

--- godot-git-plugin/src/git_api.cpp
+++ godot-git-plugin/src/git_api.cpp
@@ -7,47 +7,51 @@ namespace godot {
 void GitAPI::_register_methods() {
 
 	register_method("_process", &GitAPI::_process);
-	register_method("get_vcs_name", &GitAPI::initialize);
-	register_method("get_commit_dock_panel_container", &GitAPI::get_commit_dock_panel_container);
-	register_method("get_initialization_settings_panel_container", &GitAPI::get_initialization_settings_panel_container);
-	register_method("shut_down", &GitAPI::shut_down);
-	register_method("get_project_name", &GitAPI::get_project_name);
-	register_method("get_vcs_name", &GitAPI::get_vcs_name);
+
+	register_method("_initialiaze", &GitAPI::_initialize);
+	register_method("_get_vcs_name", &GitAPI::_get_vcs_name);
+	register_method("_get_project_name", &GitAPI::_get_project_name);
+	register_method("_get_commit_dock_panel_container", &GitAPI::_get_commit_dock_panel_container);
+	register_method("_get_initialization_settings_panel_container", &GitAPI::_get_initialization_settings_panel_container);
+	register_method("_shut_down", &GitAPI::_shut_down);
 }
 
-Control *GitAPI::get_commit_dock_panel_container() {
+Variant GitAPI::_get_commit_dock_panel_container() {
 
-	return &Control();
+	return init_settings_panel_container;
 }
 
-Control *GitAPI::get_initialization_settings_panel_container() {
+Variant GitAPI::_get_initialization_settings_panel_container() {
 
-	panel_container = new PanelContainer();
+	init_settings_panel_container = memnew(PanelContainer);
 
-	button = new Button();
-	panel_container->add_child(button);
+	init_settings_button = memnew(Button);
+	init_settings_panel_container->add_child(init_settings_button);
 
-	return panel_container;
+	return init_settings_panel_container;
 }
 
-String GitAPI::get_project_name() {
+String GitAPI::_get_project_name() {
 
 	return String("project");
 }
 
-String GitAPI::get_vcs_name() {
+String GitAPI::_get_vcs_name() {
 
 	return "Git";
 }
 
-bool GitAPI::initialize(const String project_root_path) {
+bool GitAPI::_initialize(const String project_root_path) {
 
 	return true;
 }
 
-bool GitAPI::shut_down() {
+bool GitAPI::_shut_down() {
 
-	return false;
+	memdelete(init_settings_panel_container);
+	memdelete(init_settings_button);
+
+	return true; // TODO: change with git2_shutdown() == 0
 }
 
 void GitAPI::_init() {
--- godot-git-plugin/src/git_api.h
+++ godot-git-plugin/src/git_api.h
@@ -1,11 +1,14 @@
 #ifndef GIT_API_H
 #define GIT_API_H
 
-#include <Godot.hpp>
-#include <Control.hpp>
-#include <PanelContainer.hpp>
 #include <Button.hpp>
+#include <Control.hpp>
 #include <EditorVCSInterface.hpp>
+#include <Godot.hpp>
+#include <PanelContainer.hpp>
+
+#define memnew(m_Class) new m_Class()
+#define memdelete(m_pointer) delete m_pointer
 
 namespace godot {
 
@@ -13,18 +16,18 @@ class GitAPI : public EditorVCSInterface {
 
 	GODOT_CLASS(GitAPI, EditorVCSInterface)
 
-	godot::PanelContainer *panel_container;
-	godot::Button *button;
+	godot::PanelContainer *init_settings_panel_container;
+	godot::Button *init_settings_button;
 
 public:
 	static void _register_methods();
-	bool initialize(const String project_root_path);
-	Control *get_commit_dock_panel_container();
-	Control *get_initialization_settings_panel_container();
-	bool shut_down();
 
-	String get_project_name();
-	String get_vcs_name();
+	Variant _get_commit_dock_panel_container();
+	Variant _get_initialization_settings_panel_container();
+	String _get_project_name();
+	String _get_vcs_name();
+	bool _initialize(const String project_root_path);
+	bool _shut_down();
 
 	void _init();
 	void _process();
@@ -33,6 +36,6 @@ public:
 	~GitAPI();
 };
 
-}
+} // namespace godot
 
 #endif // !GIT_API_H
