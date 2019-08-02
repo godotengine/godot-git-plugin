892f9264cdcf09357a86b25d1be5ae01517e6c4d
Twarit Waikar <wtwarit@gmail.com>
2019-08-03 01:22:16 +0530

    Add libgit2 windows lib (#4)
    * Add libgit2 libs + source
    
    * Add libgit2 binaries to demo project
    
    * Update gitignore to regard all demo files
    
    * Add windows addon binary to demo
    
    * Add git init
    
    * Fix godot_gdnative_singleton not existing warning
    
    * Fix missing godot_gdnative_singleton missing warning

--- godot-git-plugin/src/gdlibrary.cpp
+++ godot-git-plugin/src/gdlibrary.cpp
@@ -7,6 +7,9 @@ extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
 	godot::Godot::gdnative_init(o);
 }
 
+extern "C" void GDN_EXPORT godot_gdnative_singleton(godot_gdnative_init_options *o) {
+}
+
 extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o) {
 
 	godot::Godot::gdnative_terminate(o);
