bde2557be7254d06c01461b0e94bb239a6d95512
IronicallySerious <wtwarit@gmail.com>
2019-08-23 19:57:02 +0530

    Update linux binaries to fix lingering commit index

--- demo/new_script.gd
+++ demo/new_script.gd
@@ -3,7 +3,6 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
