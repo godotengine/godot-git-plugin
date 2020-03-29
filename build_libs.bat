271fc3e9967e217f67a4202a9e6ba598f03f2581
Twarit <wtwarit@gmail.com>
2020-03-30 01:20:47 +0530

    Remove CI compilaion warnings/errors

--- build_libs.bat
+++ build_libs.bat
@@ -2,10 +2,6 @@ set GODOT_PATH_RELATIVE_TO_PLUGIN="..\godot\bin"
 set GIT_PLUGIN_RELATIVE_TO_GODOT="..\..\godot-git-plugin\"
 git submodule init
 git submodule update --init --recursive
-cd %GODOT_PATH_RELATIVE_TO_PLUGIN%
-godot.windows.tools.64.exe --gdnative-generate-json-api api.json
-copy api.json %GIT_PLUGIN_RELATIVE_TO_GODOT%\api.json /Y
-cd %GIT_PLUGIN_RELATIVE_TO_GODOT%
 
 cd godot-git-plugin\thirdparty\libgit2\
 mkdir build
