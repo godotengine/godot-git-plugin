93f6e49d7e9748f574aae77c6cfd47a94bb7b59d
Twarit <wtwarit@gmail.com>
2020-05-13 23:07:49 +0530

    Apply same build options for linux static libgit2 build

--- build_libs.sh
+++ build_libs.sh
@@ -5,7 +5,7 @@ cd godot-git-plugin/thirdparty/libgit2/
 mkdir build
 cd build/
 rm CMakeCache.txt
-cmake ..
+cmake .. -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DBUILD_EXAMPLES=OFF -DUSE_SSH=OFF -DUSE_HTTPS=OFF -DUSE_BUNDLED_ZLIB=ON
 cmake --build .
 cd ../../../../
 mv godot-git-plugin/thirdparty/libgit2/build/libgit2.a demo/bin/x11/libgit2.a
