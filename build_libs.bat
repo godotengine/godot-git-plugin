6d98ee28b52c1c58a35d670ba633cc6da4cc92e4
Twarit Waikar <wtwarit@gmail.com>
2020-05-17 18:42:52 +0530

    Add missing PIC flag in while building libgit2

--- build_libs.bat
+++ build_libs.bat
@@ -5,7 +5,7 @@ cd godot-git-plugin\thirdparty\libgit2\
 mkdir build
 cd build\
 del /F CMakeCache.txt
-cmake .. -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DBUILD_EXAMPLES=OFF -DUSE_SSH=OFF -DUSE_HTTPS=OFF -DUSE_BUNDLED_ZLIB=ON -DWINHTTP=OFF
+cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DBUILD_EXAMPLES=OFF -DUSE_SSH=OFF -DUSE_HTTPS=OFF -DUSE_BUNDLED_ZLIB=ON -DWINHTTP=OFF
 cmake --build .
 cd ../../../../
 copy godot-git-plugin\thirdparty\libgit2\build\%1\git2.lib demo\bin\win64\
