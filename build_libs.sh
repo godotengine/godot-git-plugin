bd7ac760352f85db422c878d5536b933f23dacab
serious <serious@email.com>
2020-09-06 01:07:47 +0530

    Add PIC flag to libgit2 linux build

--- build_libs.sh
+++ build_libs.sh
@@ -5,12 +5,18 @@ cd godot-git-plugin/thirdparty/libgit2/
 mkdir build
 cd build/
 rm CMakeCache.txt
-cmake .. -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DBUILD_EXAMPLES=OFF -DUSE_SSH=OFF -DUSE_HTTPS=OFF -DUSE_BUNDLED_ZLIB=ON
+cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DBUILD_EXAMPLES=OFF -DUSE_SSH=OFF -DUSE_HTTPS=OFF -DUSE_BUNDLED_ZLIB=ON
 cmake --build .
 cd ../../../../
 mv godot-git-plugin/thirdparty/libgit2/build/libgit2.a demo/bin/x11/libgit2.a
 
+if [ -z "$CI" ]
+then
+	echo "Non-CI run was detected"     
+else
+	echo "CI run was detected"
+fi
+
 cd godot-cpp/;
-apt-get install scons
 scons platform=linux target=$1 generate_bindings=yes bits=64;
 cd ..
