7cea3fa0a9549ec7b8dcf1a5c661ef4494808016
Twarit Waikar <wtwarit@gmail.com>
2020-09-06 06:56:47 +0530

    Fix build_libs.bat making x11 dir on windows build

--- build_libs.bat
+++ build_libs.bat
@@ -8,7 +8,7 @@ del /F CMakeCache.txt
 cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DBUILD_EXAMPLES=OFF -DUSE_SSH=OFF -DUSE_HTTPS=OFF -DUSE_BUNDLED_ZLIB=ON -DWINHTTP=OFF
 cmake --build . --config %1
 cd ../../../../
-mkdir "demo/bin/x11/"
+mkdir "demo/bin/win64/"
 copy godot-git-plugin\thirdparty\libgit2\build\%1\git2.lib demo\bin\win64\
 
 if "%CI%"=="" (
