12ed472049a4e6a8f6b691d408819fe8825a99ff
IronicallySerious <wtwarit@gmail.com>
2020-12-09 18:13:47 +0530

    Port build scripts to Mac

---
+++ build_libs_mac.sh
@@ -0,0 +1,23 @@
+git submodule init;
+git submodule update --init --recursive;
+
+cd godot-git-plugin/thirdparty/libgit2/
+mkdir build
+cd build/
+rm CMakeCache.txt
+cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DBUILD_EXAMPLES=OFF -DUSE_SSH=OFF -DUSE_HTTPS=OFF -DUSE_BUNDLED_ZLIB=ON -DUSE_ICONV=OFF
+cmake --build . --config $1
+cd ../../../../
+mkdir -p "demo/bin/osx/"
+cp "godot-git-plugin/thirdparty/libgit2/build/libgit2.a" "demo/bin/osx/libgit2.a"
+
+if [ -z "$CI" ]
+then
+	echo "Non-CI run was detected"     
+else
+	echo "CI run was detected"
+fi
+
+cd godot-cpp/;
+scons platform=osx target=$1 generate_bindings=yes bits=64;
+cd ..
