9da28eed5a02b648b729a5775e5641a3b84ce1aa
Twarit Waikar <wtwarit@gmail.com>
2021-04-19 15:00:42 +0530

    Treat CMakeCache.txt not found while deletion as non-error

--- build_libs.sh
+++ build_libs.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive;
 cd godot-git-plugin/thirdparty/libgit2/
 mkdir build
 cd build/
-rm CMakeCache.txt
+rm -f CMakeCache.txt
 cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DBUILD_EXAMPLES=OFF -DUSE_SSH=OFF -DUSE_HTTPS=OFF -DUSE_BUNDLED_ZLIB=ON
 cmake --build . --config $1
 cd ../../../../
@@ -13,7 +13,7 @@ cp "godot-git-plugin/thirdparty/libgit2/build/libgit2.a" "demo/bin/x11/libgit2.a
 
 if [ -z "$CI" ]
 then
-	echo "Non-CI run was detected"     
+	echo "Non-CI run was detected"
 else
 	echo "CI run was detected"
 fi
