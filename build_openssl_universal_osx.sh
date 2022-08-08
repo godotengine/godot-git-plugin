c38a44d19232db5523c6c8ce84937fe7ec383d11
Twarit Waikar <wtwarit@gmail.com>
2022-08-09 03:50:00 +0530

    Extract OpenSSL libs from CI

--- build_openssl_universal_osx.sh
+++ build_openssl_universal_osx.sh
@@ -15,7 +15,6 @@ cd openssl_x86_64
 ./Configure darwin64-x86_64-cc -mmacosx-version-min=10.13
 make
 cd ../
-pwd
 lipo -create openssl_arm64/libcrypto.a openssl_x86_64/libcrypto.a -output thirdparty/openssl/libcrypto.a
 lipo -create openssl_arm64/libssl.a openssl_x86_64/libssl.a -output thirdparty/openssl/libssl.a
 rm openssl-$OPENSSL_VERSION.tar.gz
